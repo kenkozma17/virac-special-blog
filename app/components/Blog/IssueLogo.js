f802cb55597beb452fa7725d12f91822bc4bfb22
Ken Kozma <kenkozma17@gmail.com>
2023-10-03 15:31:20 +0800

    tuning styling

--- app/components/Blog/IssueLogo.js
+++ app/components/Blog/IssueLogo.js
@@ -1,7 +1,7 @@
 export default function IssueLogo(props) {
   return (
     <div className="z-50 absolute md:top-6 top-2 md:right-6 right-2 text-right">
-      <p className="leading-10 text-[1.5rem] md:text-[2.5rem] uppercase text-white">
+      <p className="md:leading-10 leading-6 text-[1.5rem] md:text-[2.5rem] uppercase text-white">
         <span className="text-vs-red">Virac</span> Special
       </p>
       <p className="tracking-wide font-merriweather-sans text-white text-xs">
